d0d10b589ad74a0bcfe04cba1abae168039a2ff6
Lima <linam.errico@gmail.com>
2023-08-30 16:12:04 -0400

    added package json and updated index file

--- index.js
+++ index.js
@@ -1,13 +1,95 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateREADME = ({projectTitle, description, installation, usage, license, contribution, tests, githubUsername, email}) =>
+`# ${projectTitle}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+## Description
+${description}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Table of Contents
+- [Description](#description)
+- [Table of Contents](#table-of-contents)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
-// Function call to initialize app
-init();
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## License
+${license} License.
+
+## Contributing
+${contribution}
+
+## Tests
+${tests}
+
+## Questions
+For any questions, you can reach me at [GitHub](https://github.com/${githubUsername}) or by email at ${email}.
+`;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'What is the title of your project:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'What is the description:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Describe the installation:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage?',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'What are the contribution guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the tests?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license:',
+      choices: ['MIT', 'Apache', 'GPL', 'GNU', 'BSD', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'githubUsername',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address:',
+    },
+  ])
+
+  .then((answers) => {
+    const readmeContent = generateREADME(answers);
+
+    fs.writeFile('README.md', readmeContent, (err) => 
+        err ? console.log(err) : console.log('Successfully created README file!')
+    ); 
+});
